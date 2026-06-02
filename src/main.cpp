5200655e79715bdcd47d1a39a06bf0e69b9addb4
Nayan <geeknayan@gmail.com>
2026-06-02 15:54:04 +0530

    kjdsjka

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "iot/IIotApp.h"
 
-/*--@Autowired--*/
-IIotAppPtr iotApp = Implementation<IIotApp>::type::GetInstance();
+/* @Autowired */
+IIotAppPtr iotApp;
 
 extern "C" void app_main(void) {
     iotApp->Start();
