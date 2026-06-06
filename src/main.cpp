eb8eeb524203e8cbff0f4c4558338873dc22ee43
Nayan <geeknayan@gmail.com>
2026-06-06 17:28:49 +0530

    kldskla

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include "iot/IIotApp.h"
 
-/*--@Autowired--*/
-IIotAppPtr iotApp = Implementation<IIotApp>::type::GetInstance();
+#include "IDeviceManager.h"
+#include "device/IDeviceCollection.h"
+
+/* @Autowired */
+IDeviceCollectionPtr deviceCollection;
+
+Void DeviceManager::Setup() {
+}
+
+Void DeviceManager::Loop() {
+    deviceCollection->RefreshAllDevices();
+}
+
+/* @Autowired */
+IIotAppPtr iotApp;
 
 extern "C" void app_main(void) {
     iotApp->Start();
